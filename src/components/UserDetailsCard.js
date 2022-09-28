8762df0a61f7640bcbb6003ef57305cb38f7d8aa
Victor Okonkwo <emmyvic98@gmail.com>
2022-09-28 14:57:13 +0100

    User Details Card added

--- src/components/UserDetailsCard.js
+++ src/components/UserDetailsCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const UserDetailsCard = ({title,value,unit}) => {
   return (
       <div className="col-lg-3 col-md-6 mb-3">
-        <div className="card bg-primary text-light">
+        <div className="card bg-primary text-light shadow">
           <div className="card-body text-center">
             <h5 className="card-title">{title}</h5>
             <p className="card-text py-3">
