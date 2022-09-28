3dbe03e81c2058e12839d347587a23a0e6de442d
Victor Okonkwo <emmyvic98@gmail.com>
2022-09-28 18:18:06 +0100

    changed details prop values

--- src/components/UserDetailsContainer.js
+++ src/components/UserDetailsContainer.js
@@ -6,10 +6,10 @@ const UserDetailsContainer = () => {
   return (
     <div className="container mt-5 px-3">
         <Row>
-            <UserDetailsCard title="Current Deposit" value="10" unit="BUSD"/>
-            <UserDetailsCard title="Current Deposit" value="10" unit="BUSD"/>
-            <UserDetailsCard title="Current Deposit" value="10" unit="BUSD"/>
-            <UserDetailsCard title="Current Deposit" value="10" unit="BUSD"/>
+            <UserDetailsCard title="Tokens Mined" value="10000" unit="VTK"/>
+            <UserDetailsCard title="Total Deposit" value="0" unit="BUSD"/>
+            <UserDetailsCard title="Profit" value="10" unit="BUSD"/>
+            <UserDetailsCard title="Referral Bonus" value="10" unit="BUSD"/>
         </Row>
     </div>
   )
